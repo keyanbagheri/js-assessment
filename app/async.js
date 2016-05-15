f58673f6646eb587ba2e15cf5de91f35eea6c080
keyanbagherin <keyanbagheri@gmail.com>
2016-05-15 12:58:39 -0400

    passing all tests for async

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+  	return new Promise(function(resolve){
+			resolve(value);
+  	})
   },
 
   manipulateRemoteData: function(url) {
-
+  	return new Promise(function(resolve){
+	  	var request = new XMLHttpRequest();
+	  	request.open('GET', url, true);
+	  	request.onload = function(){
+	  		if(this.status == 200){
+	  			var results = JSON.parse(this.responseText)["people"];
+			  	var array = []
+			  	for(i in results){
+			  		array.push(results[i]["name"])
+			  	}
+			  	resolve(array.sort());
+	  		}
+	  	};
+	  	request.send();
+  	});
   }
 };
