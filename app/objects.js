dbb667183df64d04210cdf33d4e5e592d3ad6092
keyanbagherin <keyanbagheri@gmail.com>
2016-05-15 11:13:04 -0400

    passing all tests for objects

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,19 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+  	obj.newFunc = fn
+  	return obj.newFunc()
   },
 
   alterObjects: function(constructor, greeting) {
-
+  	constructor.prototype.greeting = greeting
   },
 
   iterate: function(obj) {
-
+  	array = []
+  	Object.getOwnPropertyNames(obj).forEach(function(key){
+  		array.push(key + ": " + obj[key])
+  	})
+  	return array
   }
 };
