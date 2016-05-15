bc827019a8be3aeada4bc824b151b561cea31ea4
keyanbagherin <keyanbagheri@gmail.com>
2016-05-15 11:18:56 -0400

    passing all tests for best practices

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -9,7 +9,7 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.bestPracticesAnswers = {
   globals: function() {
-    myObject = {
+    var myObject = {
       name: 'Jory'
     };
 
@@ -17,10 +17,11 @@ exports.bestPracticesAnswers = {
   },
 
   parseInt: function(num) {
-    return parseInt(num);
+    // specify radix of 10 to represent common decimal numeral system
+    return parseInt(num, 10);
   },
 
   identity: function(val1, val2) {
-
+    return val1 === val2
   }
 };
